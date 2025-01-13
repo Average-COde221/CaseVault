63859c4a1ebec1e64da6b7fa915518f3be93a774
Average-COde221 <75667517+Average-COde221@users.noreply.github.com>
2025-01-13 15:25:51 +0530

    changed login to only accept token

--- routes/user_auth.js
+++ routes/user_auth.js
@@ -1,5 +1,7 @@
 const {auth,db}=require('../firebase');
 
+const admin=require('firebase-admin');
+
 const express=require('express');
 
 const router=express.Router();
@@ -12,8 +14,8 @@ router.post('/register', async(req, res) =>{
     const {email,password,name}=req.body;
     try {
         const userRecord=await auth.createUser({email:email,password:password,displayName:name});
-        await db.collection('users').doc(userRecord.user_id).set({email:userRecord.email,displayName:userRecord.name,createdAt:admin.firestore.FieldValue.servertimeStamp()});
-        res.status(201).json({message:'user registration successful',userId:userRecord.user_id});
+        await db.collection('users').doc(userRecord.uid).set({email:userRecord.email,displayName:userRecord.name,createdAt:admin.firestore.FieldValue.serverTimeStamp()});
+        res.status(201).json({message:'user registration successful',userId:userRecord.uid});
 
     }
     catch (err) {
@@ -25,21 +27,20 @@ router.post('/register', async(req, res) =>{
 //login api
 
 router.post('/login',async(req, res) =>{
-    const {email,password}=req.body;
+    const {idToken}=req.body;
     try {
-        const user=await auth.signInWithEmailAndPassword(email,password);
-        if (!userRecord){
-            res.status(404).json({error:'user not found'});
-        }
+        const decodedToken=await auth.verifyIdToken(idToken);
+        const userId=decodedToken.uid;
         
-        const customToken= await auth.createCustomToken(userRecord.user_id);
+        
 
-        res.status(200).json({message:'login successful',token:customToken});
+        res.status(200).json({message:'login successful',userId: userId,userDetails:decodedToken});
+
 
 
     } catch (err) {
-        console.error('error logging in user:',err);
-        res.status(400).json({error:err.message});
+        console.error('error verifying id token:',err.message);
+        res.status(401).json({error:'error verifying id token:'});
     }
     
     
